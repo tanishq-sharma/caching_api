2f4de3678521d2461b353b32b66359f267e5baeb
tanishq sharma <tanishqsharma0798@gmail.com>
2020-04-20 19:50:58 +0530

    script for posting data

---
+++ scripts/postData.py
@@ -0,0 +1,24 @@
+import requests
+url = 'http://localhost:8080/cache/set'
+payload = {'ID':'1','Value':'first value'}
+
+#r = requests.get(url)
+
+#r = requests.get(url,params=payload)
+
+r=requests.post(url,data=payload)
+
+import json
+r = requests.post(url, data = json.dumps(payload))
+payload = {'ID':'2','Value':'2 value'}
+r = requests.post(url, data = json.dumps(payload))
+payload = {'ID':'3','Value':'3 value'}
+r = requests.post(url, data = json.dumps(payload))
+payload = {'ID':'4','Value':'4 value'}
+r = requests.post(url, data = json.dumps(payload))
+payload = {'ID':'5','Value':'5 value'}
+r = requests.post(url, data = json.dumps(payload))
+payload = {'ID':'6','Value':'6 value'}
+r = requests.post(url, data = json.dumps(payload))
+r.text
+r.status_code
